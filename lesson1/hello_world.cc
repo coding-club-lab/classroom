a33aabd6687609e0e872f0548e5d58d52392164b
Yuhong Wu <mere.refund.5o@icloud.com>
2025-01-18 20:48:28 -0600

    second commit

--- lesson1/hello_world.cc
+++ lesson1/hello_world.cc
@@ -6,4 +6,4 @@ int main() {
     std::cin>>name;
     std::cout<<"Hello "<<name<<std::endl;
     return(0);
-}
+} 
--- lesson1/hello_world.py
+++ lesson1/hello_world.py
@@ -1,4 +1,3 @@
 print('Your name?')
 name=input()
-print(f'Hello {name}')
-print('test')
+print(f'Hello {name}') 
